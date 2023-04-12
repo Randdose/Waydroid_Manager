d30a664ee5996c5d8d3f712ce9f424bf5f824080
Randdose <randomnes037@gmail.com>
2023-04-12 07:20:48 +0400

    added way to ask for root privilege to run a command

--- SysTools.py
+++ SysTools.py
@@ -5,8 +5,11 @@ def commandOutput(command):
 	return ((subprocess.check_output(command, shell=True)).decode())[:-1]
 
 # Runs a Bash command
-def runCommand(command):
-	subprocess.run(command, shell=True)
+def runCommand(command, priv=0):
+	if (priv==1):
+		subprocess.run(f"pkexec {command}", shell=True)
+	else:
+		subprocess.run(command, shell=True)
 
 # Checks if a package is installed
 def pkgIsInstalled(pkgName):
